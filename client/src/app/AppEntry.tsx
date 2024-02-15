aca5113c27b53990baea9eaf8de22081a510feaf
Evgeniy <KworkEvgeniy@yandex.ru>
2024-02-15 20:58:14 +0300

    Создана стилизация хедера и функционал popup окна.

--- client/src/app/AppEntry.tsx
+++ client/src/app/AppEntry.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../shared/index.css';
+import '../shared/index.scss';
 import { App } from './layouts/BaseLayout.tsx';
 import { Provider } from 'react-redux';
 import { store } from '../app/AppStore.ts';
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
-        <React.StrictMode>
+        <BrowserRouter>
             <App />
-        </React.StrictMode>
+        </BrowserRouter>
     </Provider>
-
 );
