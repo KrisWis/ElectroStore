d1df3c8d8e7fa501891e43f9686947c633ec68f4
Evgeniy <KworkEvgeniy@yandex.ru>
2024-02-18 22:20:40 +0300

    Добавлен Smooth Scrolling.

--- client/src/app/layouts/BaseLayout.tsx
+++ client/src/app/layouts/BaseLayout.tsx
@@ -1,11 +1,13 @@
 import './App.module.scss';
 import { Main } from '../../pages';
+import SmoothScroll from '../../features/SmoothScroll/ui';
 
 export const App: React.FC = (): React.JSX.Element => {
 
     return (
         <>
             <Main />
+            <SmoothScroll />
         </>
     )
 }
--- client/src/features/index.ts
+++ client/src/features/index.ts
@@ -1,3 +1,4 @@
 export * from './NavBar/ui/index';
 export * from './Header_icons/ui/index';
 export * from './Search_Modal/ui/index';
+export * from './SmoothScroll/ui/index';
