50fab53977c0c27e096ba3676d9c907b69617d19
Evgeniy <KworkEvgeniy@yandex.ru>
2024-03-03 18:06:40 +0300

    Updates

--- assets/index-ZSqtZkS2.js
+++
@@ -1,1 +0,0 @@
-import{j as s,h as a}from"./index-CrjKKHGF.js";const t=()=>s.jsxs("div",{className:a.background_animation,children:[s.jsx("span",{}),s.jsx("span",{}),s.jsx("span",{}),s.jsx("span",{}),s.jsx("span",{})]});export{t as default};
