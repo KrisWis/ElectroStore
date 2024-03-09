034777a5f18961a2fb2ef13f991eec90ec5b823c
Evgeniy <KworkEvgeniy@yandex.ru>
2024-03-09 22:55:15 +0300

    Updates

---
+++ assets/index-DipqCEQL.js
@@ -0,0 +1,1 @@
+import{j as s,h as a}from"./index-CcKgoPmp.js";const t=()=>s.jsxs("div",{className:a.background_animation,children:[s.jsx("span",{}),s.jsx("span",{}),s.jsx("span",{}),s.jsx("span",{}),s.jsx("span",{})]});export{t as default};
