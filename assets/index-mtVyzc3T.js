f8dc9ffd877fde08e9bd3499e16f20e8bb3428a7
Evgeniy <KworkEvgeniy@yandex.ru>
2024-03-09 22:43:55 +0300

    Updates

---
+++ assets/index-mtVyzc3T.js
@@ -0,0 +1,1 @@
+import{j as s,h as a}from"./index-CQQu2zNQ.js";const t=()=>s.jsxs("div",{className:a.background_animation,children:[s.jsx("span",{}),s.jsx("span",{}),s.jsx("span",{}),s.jsx("span",{}),s.jsx("span",{})]});export{t as default};
