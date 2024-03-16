e9140b12be6c43a83ee55911b7eb68cf094fe8f3
Evgeniy <KworkEvgeniy@yandex.ru>
2024-03-16 10:50:43 +0300

    Добавление HTTP запросов на сервер.

--- client/src/widgets/Our_New_Arrivals/helpers/index.ts
+++ client/src/widgets/Our_New_Arrivals/helpers/index.ts
@@ -1,34 +1,43 @@
 import { WidgetItemProps } from "../../../entities/Widget_item/types";
+import axios, { AxiosResponse } from 'axios';
 
-export const OurNewArrivals_items_props: WidgetItemProps[] = [
-    {
-        id: 1,
-        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--1.webp',
-        caption: 'Phone Cover 5.6 inches',
-        price: '95.00'
-    },
-    {
-        id: 2,
-        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--2.webp',
-        caption: 'Smart Phone 128GB RAM',
-        price: '55.00'
-    },
-    {
-        id: 3,
-        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--3.webp',
-        caption: 'Smart Phone 128GB RAM',
-        price: '55.00'
-    },
-    {
-        id: 4,
-        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--4.webp',
-        caption: 'Smart Phone 128GB RAM',
-        price: '55.00'
-    },
-    {
-        id: 5,
-        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--5.webp',
-        caption: '22 Inch LED Display',
-        price: '65.00'
-    },
-]
+export let OurNewArrivals_items_props: WidgetItemProps[];
+
+try {
+    const response: AxiosResponse = await axios.get(`http://127.0.0.1:8000/ElectroStore/api/goods`);
+    OurNewArrivals_items_props = response.data;
+
+} catch {
+    OurNewArrivals_items_props = [
+        {
+            id: 1,
+            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--1.webp',
+            caption: 'Phone Cover 5.6 inches',
+            price: '95.00'
+        },
+        {
+            id: 2,
+            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--2.webp',
+            caption: 'Smart Phone 128GB RAM',
+            price: '55.00'
+        },
+        {
+            id: 3,
+            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--3.webp',
+            caption: 'Smart Phone 128GB RAM',
+            price: '55.00'
+        },
+        {
+            id: 4,
+            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--4.webp',
+            caption: 'Smart Phone 128GB RAM',
+            price: '55.00'
+        },
+        {
+            id: 5,
+            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--5.webp',
+            caption: '22 Inch LED Display',
+            price: '65.00'
+        },
+    ];
+}
