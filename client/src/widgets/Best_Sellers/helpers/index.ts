abea62322e11aa5a10b10e6224b556ebb125bf1e
Evgeniy <KworkEvgeniy@yandex.ru>
2024-03-16 11:30:18 +0300

    Перенос axios запросов на GraphQL.

--- client/src/widgets/Best_Sellers/helpers/index.ts
+++ client/src/widgets/Best_Sellers/helpers/index.ts
@@ -1,63 +1,76 @@
 import { WidgetItemProps } from "../../../entities/Widget_item/types";
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
+import { gql } from 'graphql-tag';
 
-export let BestSellers_items_props: WidgetItemProps[];
+export let BestSellers_items_props: WidgetItemProps[] = [
+    {
+        id: 1,
+        imageURL: 'images/BestSellers_items/BestSellers_item--1.webp',
+        caption: 'Phone Cover 5.6 inches',
+        price: '95.00'
+    },
+    {
+        id: 2,
+        imageURL: 'images/BestSellers_items/BestSellers_item--2.webp',
+        caption: 'Smart Phone 128GB RAM',
+        price: '55.00'
+    },
+    {
+        id: 3,
+        imageURL: 'images/BestSellers_items/BestSellers_item--3.webp',
+        caption: 'Smart Phone 128GB RAM',
+        price: '55.00'
+    },
+    {
+        id: 4,
+        imageURL: 'images/BestSellers_items/BestSellers_item--4.webp',
+        caption: 'Phone Cover 5.6 inches',
+        price: '95.00'
+    },
+    {
+        id: 5,
+        imageURL: 'images/BestSellers_items/BestSellers_item--5.webp',
+        caption: 'Smart Phone 128GB RAM',
+        price: '55.00'
+    },
+    {
+        id: 6,
+        imageURL: 'images/BestSellers_items/BestSellers_item--6.webp',
+        caption: 'Smart Phone 128GB RAM',
+        price: '55.00'
+    },
+    {
+        id: 7,
+        imageURL: 'images/BestSellers_items/BestSellers_item--7.webp',
+        caption: 'Smart Phone 128GB RAM',
+        price: '55.00'
+    },
+    {
+        id: 8,
+        imageURL: 'images/BestSellers_items/BestSellers_item--8.webp',
+        caption: '22 Inch LED Display',
+        price: '65.00'
+    },
+];
 
-try {
+const query = gql`
+  query {
+    goods {
+      id
+      name
+      price
+      imageURL
+    }
+  }
+`;
 
-    const response: AxiosResponse = await axios.get(`http://127.0.0.1:8000/ElectroStore/api/goods`);
+axios({
+    url: 'http://127.0.0.1:8000/ElectroStore/api/goods',
+    method: 'post',
+    data: {
+        query: query
+    }
+
+}).then(response => {
     BestSellers_items_props = response.data;
-
-} catch {
-
-    BestSellers_items_props = [
-        {
-            id: 1,
-            imageURL: 'images/BestSellers_items/BestSellers_item--1.webp',
-            caption: 'Phone Cover 5.6 inches',
-            price: '95.00'
-        },
-        {
-            id: 2,
-            imageURL: 'images/BestSellers_items/BestSellers_item--2.webp',
-            caption: 'Smart Phone 128GB RAM',
-            price: '55.00'
-        },
-        {
-            id: 3,
-            imageURL: 'images/BestSellers_items/BestSellers_item--3.webp',
-            caption: 'Smart Phone 128GB RAM',
-            price: '55.00'
-        },
-        {
-            id: 4,
-            imageURL: 'images/BestSellers_items/BestSellers_item--4.webp',
-            caption: 'Phone Cover 5.6 inches',
-            price: '95.00'
-        },
-        {
-            id: 5,
-            imageURL: 'images/BestSellers_items/BestSellers_item--5.webp',
-            caption: 'Smart Phone 128GB RAM',
-            price: '55.00'
-        },
-        {
-            id: 6,
-            imageURL: 'images/BestSellers_items/BestSellers_item--6.webp',
-            caption: 'Smart Phone 128GB RAM',
-            price: '55.00'
-        },
-        {
-            id: 7,
-            imageURL: 'images/BestSellers_items/BestSellers_item--7.webp',
-            caption: 'Smart Phone 128GB RAM',
-            price: '55.00'
-        },
-        {
-            id: 8,
-            imageURL: 'images/BestSellers_items/BestSellers_item--8.webp',
-            caption: '22 Inch LED Display',
-            price: '65.00'
-        },
-    ];
-}
+})
--- client/src/widgets/Our_New_Arrivals/helpers/index.ts
+++ client/src/widgets/Our_New_Arrivals/helpers/index.ts
@@ -1,43 +1,57 @@
 import { WidgetItemProps } from "../../../entities/Widget_item/types";
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
+import { gql } from 'graphql-tag';
 
-export let OurNewArrivals_items_props: WidgetItemProps[];
+export let OurNewArrivals_items_props: WidgetItemProps[] = [
+    {
+        id: 1,
+        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--1.webp',
+        caption: 'Phone Cover 5.6 inches',
+        price: '95.00'
+    },
+    {
+        id: 2,
+        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--2.webp',
+        caption: 'Smart Phone 128GB RAM',
+        price: '55.00'
+    },
+    {
+        id: 3,
+        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--3.webp',
+        caption: 'Smart Phone 128GB RAM',
+        price: '55.00'
+    },
+    {
+        id: 4,
+        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--4.webp',
+        caption: 'Smart Phone 128GB RAM',
+        price: '55.00'
+    },
+    {
+        id: 5,
+        imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--5.webp',
+        caption: '22 Inch LED Display',
+        price: '65.00'
+    },
+];;
+const query = gql`
+  query {
+    goods {
+      id
+      name
+      price
+      imageURL
+    }
+  }
+`;
 
-try {
-    const response: AxiosResponse = await axios.get(`http://127.0.0.1:8000/ElectroStore/api/goods`);
+axios({
+    url: 'http://127.0.0.1:8000/ElectroStore/api/goods',
+    method: 'post',
+    data: {
+        query: query
+    }
+
+}).then(response => {
     OurNewArrivals_items_props = response.data;
-
-} catch {
-    OurNewArrivals_items_props = [
-        {
-            id: 1,
-            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--1.webp',
-            caption: 'Phone Cover 5.6 inches',
-            price: '95.00'
-        },
-        {
-            id: 2,
-            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--2.webp',
-            caption: 'Smart Phone 128GB RAM',
-            price: '55.00'
-        },
-        {
-            id: 3,
-            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--3.webp',
-            caption: 'Smart Phone 128GB RAM',
-            price: '55.00'
-        },
-        {
-            id: 4,
-            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--4.webp',
-            caption: 'Smart Phone 128GB RAM',
-            price: '55.00'
-        },
-        {
-            id: 5,
-            imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--5.webp',
-            caption: '22 Inch LED Display',
-            price: '65.00'
-        },
-    ];
-}
+})
