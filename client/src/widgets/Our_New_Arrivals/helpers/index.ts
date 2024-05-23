c1b43770d08eabac0fb2ca2faac72ae909df2fc1
Evgeniy <KworkEvgeniy@yandex.ru>
2024-05-23 23:12:30 +0300

    Adding dist

--- client/src/widgets/Our_New_Arrivals/helpers/index.ts
+++ client/src/widgets/Our_New_Arrivals/helpers/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { gql } from 'graphql-tag';
-import { WidgetItemProps } from '../../../entities/Widget_item/types';
+import { WidgetItemProps } from '../../../features/Widget_item/types';
 
 /* Получение данных с апи */
 export const Our_New_Arrivals_fetch = (setOurNewArrivals_items_props: React.Dispatch<React.SetStateAction<WidgetItemProps[]>>, setIsLoading: React.Dispatch<React.SetStateAction<boolean>>): void => {
@@ -33,31 +33,36 @@ export const Our_New_Arrivals_fetch = (setOurNewArrivals_items_props: React.Disp
                 id: 1,
                 imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--1.webp',
                 caption: 'Phone Cover 5.6 inches',
-                price: '95.00'
+                price: 95.00,
+                description: "test",
             },
             {
                 id: 2,
                 imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--2.webp',
                 caption: 'Smart Phone 128GB RAM',
-                price: '55.00'
+                price: 55.00,
+                description: "test",
             },
             {
                 id: 3,
                 imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--3.webp',
                 caption: 'Smart Phone 128GB RAM',
-                price: '55.00'
+                price: 55.00,
+                description: "test",
             },
             {
                 id: 4,
                 imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--4.webp',
                 caption: 'Smart Phone 128GB RAM',
-                price: '55.00'
+                price: 55.00,
+                description: "test",
             },
             {
                 id: 5,
                 imageURL: 'images/OurNewArrivals_items/OurNewArrivals_item--5.webp',
                 caption: '22 Inch LED Display',
-                price: '65.00'
+                price: 65.00,
+                description: "test",
             },
         ]);
 
