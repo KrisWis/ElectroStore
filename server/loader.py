4161a0a26983609536b9cecafdc034f8cd0dacfb
CanICallYouJim <129004917+CanICallYouJim@users.noreply.github.com>
2024-06-12 18:51:12 +0300

    Localhost CORS added

--- server/loader.py
+++ server/loader.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .src.endpoints import router
 
 
@@ -8,3 +10,22 @@ app = FastAPI(
 )
 
 app.include_router(router)
+
+
+origins = [
+    "http://localhost:5173/ElectroStore/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/")
+async def start():
+    return "Hey there! You've done some cringe here. It's okay, we're gonna forget about it :)"
+
